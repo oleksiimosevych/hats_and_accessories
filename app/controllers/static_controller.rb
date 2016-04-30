ae28ea1cf0926a6617d8622e8c23eb8ee530f30c
regosso <regosso@ukr.net>
2016-04-30 18:18:57 +0300

    root path has all products week best path has best of week true now

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController #Ми створили нові ЕКШЕНИ!!! для статичних сторінок
   def index
     #what is wrong here? must display the best of week goods!
-    @products=Product.where(the_best_of_week: true).paginate(page: params[:page], per_page: 12)
+    @products=Product.paginate(page: params[:page], per_page: 12)
   end
   def about
   end
