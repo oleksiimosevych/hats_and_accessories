e95537b5752a863292957f4b4a881a049f9c56d1
regosso <regosso@ukr.net>
2016-05-14 15:06:06 +0300

    tryied to add gallery failed...

--- app/controllers/cart_item2s_controller.rb
+++ app/controllers/cart_item2s_controller.rb
@@ -4,10 +4,10 @@ class CartItem2sController < ApplicationController
 #it musted be "cart_items" but I have already the same named migration so I`ve called it like this
   	current_cart.cart_item2s.create!(params[:cart_item2])
     flash[:notice] = "Product added to cart"
-    session[:last_product_page] = request.env['HTTP_REFERER'] || products_url
+    session[:last_product_page] = request.env['HTTP_REFERER'] || products_path
     #I don`t know is it true or not but  it was in casts ??
-    redirect_to current_cart_url
-
+    redirect_to current_cart_path
+#_url renamed to _path
   end
 end
 #renamed from cart_items_controller.rb 
